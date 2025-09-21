bf7466b598e239e214714498fce6c09b6a4aa10f
marcin.mrugas <marcin.mrugas@allegro.com>
2025-09-21 10:30:44 +0200

    Add perspective

--- trace.py
+++ trace.py
@@ -1,30 +1,36 @@
+from math import tan
+
 from matplotlib import pyplot as plt
 import numpy as np
 
 from collisions import intersect
 from models import Camera, Sphere, Ray
 
-WIDTH = 28
-HEIGHT = 20
+WIDTH = 280
+HEIGHT = 200
 
 
 def render(camera, objects):
-    img = np.zeros((HEIGHT, WIDTH, 3))
 
-    y_offset = round(HEIGHT / 2)
-    x_offset = round(WIDTH / 2)
-    for y, pos_y in enumerate(range(-y_offset, y_offset)):
-        for x, pos_x in enumerate(range(-x_offset, x_offset)):
-            ray = Ray()
-            ray.orig = camera.pos
-            ray.dir = camera.dir
-            ray.orig = ray.orig + np.array([pos_y, pos_x, 0.0])
+    framebuffer = np.zeros((HEIGHT, WIDTH, 3))
+    aspect_ratio = WIDTH / float(HEIGHT)
+    scale = tan(np.deg2rad(camera.fov * 0.5))
+
+    for y_pixel in range(HEIGHT):
+        for x_pixel in range(WIDTH):
+
+            x = (2 * (y_pixel + 0.5) / float(WIDTH) - 1) * aspect_ratio * scale
+            y = (1 - 2 * (x_pixel + 0.5) / float(HEIGHT)) * scale
+            direction = np.array([x, y, 1])
+            direction = direction / np.linalg.norm(direction)
+            ray = Ray(camera.pos, direction)
+
 
             for obj in objects:
                 if test_collision(ray, obj):
-                    img[y][x] = obj.color
+                    framebuffer[y_pixel][x_pixel] = obj.color
 
-    return img
+    return framebuffer
 
 def test_collision(ray, obj):
     if isinstance(obj, Sphere):
@@ -33,7 +39,7 @@ def test_collision(ray, obj):
 
 
 def main():
-    camera = Camera(pos=np.array([0.0, 0.0, -15.0]))
+    camera = Camera(pos=np.array([0.0, 0.0, -35.0]))
 
     sphere1 = Sphere(radius=9.5, color=np.array([1.0, 1.0, 0.0]))
 
